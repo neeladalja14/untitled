907fa0f9861e2c67d8824549d1b06701b06e2fdd
neeladalja14 <nadal001@plattsburgh.edu>
2019-09-17 02:35:08 -0700

    variables use is shown in this piece of code.

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int myName = 5;
+    int myLastname = 10;
+    int totalName = myName + myLastname;
+
+    cout << "the name is number " << myName << endl;
+    cout << "the lastname number " << myLastname << endl;
+    cout << "total number = " << totalName << endl;
+
+    return 0;
+}
+
+
