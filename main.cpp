bc52d72b4c6ed86817d1e434eaa6c3bbc02db884
neeladalja14 <nadal001@plattsburgh.edu>
2019-09-20 21:43:00 -0700

    this code is for simple if and else example.

--- main.cpp
+++ main.cpp
@@ -2,15 +2,27 @@
 using namespace std;
 
 int main(){
-    int myName = 5;
-    int myLastname = 10;
-    int totalName = myName + myLastname;
+    cout <<"press the following numbers for thee required selection: "<< endl;
+    cout <<"1. addd records" << endl;
+    cout <<"2. for deleting records: "<< endl;
+    cout <<"3. view records: " << endl;
+    cout <<"4. search records: " << endl;
+    cout <<"5. quit...." << endl;
 
-    cout << "the name is number " << myName << endl;
-    cout << "the lastname number " << myLastname << endl;
-    cout << "total number = " << totalName << endl;
+    cout << "enter the number for your selection: "<< flush;
+    int value;
+    cin >> value;
+    if (value < 3) {
+        cout <<"sorry the election is not allowed for you"<< endl;
+    }
+    else {
+        cout<<"welcome to the database"<<endl;
+    }
 
-    return 0;
+    if (value==5){
+        cout<<"quiting the program "<<endl;
+    }
+    else {
+        cout<<"program is still working "<<endl;
+    }
 }
-
-
